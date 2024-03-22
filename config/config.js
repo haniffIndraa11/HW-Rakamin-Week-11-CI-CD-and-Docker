f7c941256f3ed160603e3b1e1a0d3211e429f615
haniff-tech <ramadhan28123@gmail.com>
2024-03-23 06:12:43 +0700

    unit testing

--- config/config.js
+++ config/config.js
@@ -15,6 +15,22 @@ const config = {
         host: 'localhost',
         dialect: 'postgres'
     },
+    docker: {
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        host: process.env.DB_HOST,
+        dialect: "postgres",
+        port: 5439
+    },
+    docker_test: {
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.DB_TEST,
+        host: process.env.DB_HOST,
+        dialect: "postgres",
+        port: 5439
+    },
 }
 
 module.exports = config
