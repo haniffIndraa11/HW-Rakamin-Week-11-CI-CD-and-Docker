54d4be864d93654dc13a9905b08e67295ad46ee5
haniff-tech <ramadhan28123@gmail.com>
2024-03-23 21:32:24 +0700

    unit testing

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@ const path = require('path')
 route.use('/api/todo', todoRoute)
 
 module.exports = route
+
+// Error saat melakukan docker compose up, jadinya image yg dibuat tidak mau run
+
+// Jadi saya hanya melakukan unit testing menggunakan Jest dan proses CI/CD
